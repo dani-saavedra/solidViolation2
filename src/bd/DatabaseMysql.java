057f481480c8005190e63aefe6fdb5637f8d72f8
= <=>
2024-11-15 20:54:03 -0500

    feat: refactor applying solid

--- src/bd/DatabaseMysql.java
+++ src/bd/DatabaseMysql.java
@@ -1,10 +1,24 @@
 package bd;
 
-public class DatabaseMysql {
+import modelo.Order;
+import servicios.OrderService;
+import servicios.OrderServiceImpl;
 
-    public void guardar() {
-        System.out.println("Creo conexion a la base de datos");
+public class DatabaseMysql implements DataSource {
+    private static DataSource instance;
+
+    public static synchronized DataSource getInstance() {
+        if (instance == null) {
+            instance = new DatabaseMysql().setNewConnection();
+        }
+        return instance;
+    }
+
+    public synchronized DataSource setNewConnection(){
+        //setea nueva conexion
+        return this;
+    }
+    public void save(Order order) {
         System.out.println("Guarde en la base de datos");
-        //Abril estuvo aca otra vez :)
     }
 }
